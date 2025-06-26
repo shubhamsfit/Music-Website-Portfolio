ec7f6e0788dc11cdc41896f15d7e54958adf2c39
shubhamsfit <shubham.kohli@simplefixit.com>
2025-06-26 18:18:27 +0530

    all files updated

--- js/myfun.js
+++ js/myfun.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 
   function changeVideo(videoId) {
     const player = document.getElementById('yt-player');
     player.src = `https://www.youtube.com/embed/${videoId}`;
   }
 
-=======
-
-  function changeVideo(videoId) {
-    const player = document.getElementById('yt-player');
-    player.src = `https://www.youtube.com/embed/${videoId}`;
-  }
-
-
->>>>>>> origin/staging
